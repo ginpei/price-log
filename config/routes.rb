e3c75e4ed91258348c7af006526d2f187f81c7c2
ginpei <ginpei@gmail.com>
2015-05-19 01:38:48 -0700

    item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :items
+
   get '/auth/:provider/callback', to: 'sessions#callback'
   post '/auth/:provider/callback', to: 'sessions#callback'
   get '/logout', to: 'sessions#destroy', as: :logout
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519033036) do
+ActiveRecord::Schema.define(version: 20150519083029) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "unit"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
